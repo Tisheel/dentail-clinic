51fce82fc645d77d26aaa12e95d70fde0dfbb8ac
tisheelbashyam <tisheel.bashyam@zopping.com>
2026-03-26 22:39:00 +0530

    Optimized Dockerfile and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -e
 echo "Pull latest code..."
 git pull origin main
 
-echo "Rebuild & restart..."
-docker-compose down
-docker-compose up -d --build
+echo "Rebuild only app services..."
+
+docker-compose up -d --build backend frontend nginx
 
 echo "Cleaning old images..."
 docker image prune -f
